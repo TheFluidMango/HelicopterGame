cb46a2bb7d4c4e3e5770629860ab83396e2ce509
Adam Smith <TheFluidMango@Adams-MacBook-Pro.local>
2016-03-18 12:31:14 -0700

    added ability for civilians to be dropped off and move to shelter

--- Helicopter Test/Assets/Scripts/unloadCivs.cs
+++ Helicopter Test/Assets/Scripts/unloadCivs.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class unloadCivs : MonoBehaviour {
 	public GameObject releasePoint;
 	public Collector collector;
+	public float distance;
+	public Transform target;
 
 	// Use this for initialization
 	void Start () {
@@ -12,11 +14,23 @@ public class unloadCivs : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-
+		if (Vector3.Distance (transform.position, target.position) < distance) {
+			if (collector != null && releasePoint != null) {
+				GameObject objectToRelease = collector.getNext ();
+				if (objectToRelease != null) {
+					Rigidbody civilian = objectToRelease.GetComponent<Rigidbody>();
+					civilian.transform.position = releasePoint.transform.position;
+					Transform dropPoint = releasePoint.transform.parent;
+					civilian.velocity = dropPoint.TransformDirection (Vector3.forward * 2);
+				}
+			} else {
+				Debug.Log ("Error");
+			}
+		}
 	}
 
 	void OnCollisionEnter(Collision other) {
-		if (other.gameObject.name.StartsWith ("HelicopterModel")) {
+		/*if (other.gameObject.name.StartsWith ("HelicopterModel")) {
 			if (collector != null && releasePoint != null) {
 				GameObject objectToRelease = collector.getNext ();
 				if (objectToRelease != null) {
@@ -28,7 +42,7 @@ public class unloadCivs : MonoBehaviour {
 			} else {
 				Debug.Log ("Error");
 			}
-		}
+		}*/
 	}
 
 
