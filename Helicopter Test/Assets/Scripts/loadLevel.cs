bcfa17f8ddf524024b236408c525b7a41d6010a9
Adam Smith <TheFluidMango@Adams-MacBook-Pro.local>
2016-05-04 20:17:00 -0700

    final

---
+++ Helicopter Test/Assets/Scripts/loadLevel.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class loadLevel : MonoBehaviour {
+
+	public void GoToLevel (string level) {
+		Application.LoadLevel (level);
+	}
+
+	public void exit () {
+		Application.Quit();
+	}
+}
